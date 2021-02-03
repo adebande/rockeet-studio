d41c7aceec992f65d1980d689654486a1f8f50a7
Aloïs Debande <63793809+adebande@users.noreply.github.com>
2021-02-03 15:20:02 +0100

    env config

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    env: {
+      EMAILJS_TEMPLATEID: process.env.EMAILJS_TEMPLATEID,
+      EMAILJS_SERVICEID: process.env.EMAILJS_SERVICEID,
+      EMAILJS_USERID: process.env.EMAILJS_USERID,
+    },
+  }
