a67514ac16a3593089fa439d2beca989eaf4b71d
Aloïs Debande <63793809+adebande@users.noreply.github.com>
2021-02-03 16:35:55 +0100

    setup google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,25 @@ export default class MyDocument extends Document {
       <Html lang="fr">
         <Head>
         <meta charset="UTF-8"/>
+        <meta property="og:url" content="https://rockeet.io" />
+        <meta property="og:title" content="Rockeet.io"/>
+        <meta property="og:description" content="Développement de sites internet, applications web et mobile." />
+        <meta property="og:image" content="https://rockeet.io/fb-img.png"/>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-P32ZZQ0X08"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-P32ZZQ0X08');
+              `,
+          }}
+        />
         </Head>
         <body>
           <Main />
